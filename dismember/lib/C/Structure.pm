c6f7b0dae2db0bca842ea674314f5edf5cc51a83
Denis Efremov <efremov@ispras.ru>
2014-03-31 14:26:07 +0400

    structure get_fields

--- dismember/lib/C/Structure.pm
+++ dismember/lib/C/Structure.pm
@@ -4,6 +4,36 @@ use Moose;
 
 extends 'C::Entity';
 
+use re '/aa';
+
+
+sub _argname
+{
+   if ($_[0] =~ m/(?|([a-zA-Z_]\w*)(?:\[[^\]]+\]|:\d+)?\h*+\Z|\(\h*\*\h*([a-zA-Z_]\w*)\h*\)\h*\()/) {
+      return $1
+   }
+   undef
+}
+
+
+sub get_fields
+{
+   my $self = shift;
+   my $code = $self->code;
+   my ($begin, $end) = (index($code, '{') + 1, rindex($code, '}'));
+
+   $code = substr($code, $begin, $end - $begin);
+
+   my @fields;
+   foreach(split(/;/, $code)) {
+      next if m/\A\s*\z/;
+
+      push @fields, _argname($_)
+   }
+
+   \@fields
+}
+
 
 __PACKAGE__->meta->make_immutable;
 
