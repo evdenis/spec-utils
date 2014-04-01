23244c5c4cad9975c0770c32cc15f74d8431c168
Denis Efremov <yefremov.denis@gmail.com>
2014-04-02 00:29:48 +0400

    лишняя проверка при парсинге глобальных переменных

--- dismember/lib/C/GlobalSet.pm
+++ dismember/lib/C/GlobalSet.pm
@@ -21,10 +21,7 @@ sub parse_global
    my %globals;
 
    while ($_[1] =~ m/extern${s}++([^;}{]+?)(?<name>[a-zA-Z_]\w*)${s}*+;/gp) {
-      my $name = $+{name};
-
-      carp("Repeated definition of global variable $name") if exists $globals{$name};
-      $globals{$name} = ${^MATCH};
+      $globals{$+{name}} = ${^MATCH}
    }
 
    return $_[0]->new(set => [ map {C::Global->new(name => $_, code => $globals{$_})} keys %globals ]);
