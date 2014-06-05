b0f3f2136aec06b71725089795dbb32c5b335522
Denis Efremov <efremov@ispras.ru>
2014-06-05 16:33:34 +0400

    kernel directory check functions

---
+++ lib/Local/Kernel/Common.pm
@@ -0,0 +1,68 @@
+package Local::Kernel::Common;
+
+use strict;
+use warnings;
+
+use feature qw(state);
+use Perl6::Junction qw(any);
+use File::Spec::Functions qw(catfile);
+use Exporter qw(import);
+
+our @EXPORT_OK = qw(check_kernel_dir check_kernel_files_readable autodetect_kernel_directory);
+
+
+sub check_kernel_dir ($)
+{
+   state %kerneldir_cache;
+
+   return $kerneldir_cache{$_[0]}
+      if exists $kerneldir_cache{$_[0]};
+
+   my $res = 0;
+
+   if ( -d $_[0] ) {
+      opendir my $kdir, $_[0] or goto OUT;
+         my @files = readdir $kdir;
+      closedir $kdir;
+
+      #Check for standard files
+      foreach (qw(Kbuild Kconfig MAINTAINERS Makefile drivers include arch kernel security)) {
+         goto OUT
+            unless $_ eq any(@files);
+      }
+
+      $res = 1;
+   }
+
+OUT:
+   $kerneldir_cache{$_[0]} = $res;
+   $res
+}
+
+sub check_kernel_files_readable ($@)
+{
+   my ($dir, @files) = @_;
+
+   if (check_kernel_dir($_[0])) {
+      foreach(@files) {
+         return 0
+            unless -r catfile($dir, $_)
+      }
+
+      return 1
+   }
+
+   0
+}
+
+sub autodetect_kernel_directory
+{
+   foreach (@{ $_{dirs} }, '.', $ENV{CURRENT_KERNEL}, 'linux', 'linux-stable', 'kernel', 'kernel-stable') {
+      return $_
+         if check_kernel_files_readable($_, @{ $_{files} })
+   }
+
+   undef
+}
+
+1;
