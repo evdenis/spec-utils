da76272d4b92aad522e42e21856b13495365c810
Denis <yefremov.denis@gmail.com>
2014-08-25 19:46:13 +0400

    Global to_string

--- lib/C/Global.pm
+++ lib/C/Global.pm
@@ -4,6 +4,19 @@ use namespace::autoclean;
 
 extends 'C::Entity';
 
+sub to_string
+{
+   my $code = $_[0]->code;
+
+   if ((my $i = index($code, '=')) != -1) {
+      $code = substr($code, 0, $i) . ';';
+      $code =~ s/^static/extern/;
+   }
+
+   $code
+}
+
+
 __PACKAGE__->meta->make_immutable;
 
 1;
