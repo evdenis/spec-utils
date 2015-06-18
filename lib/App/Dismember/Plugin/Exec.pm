ed89067c263f1806bfa7f8c55cd4a9d2f6cae39e
Denis Efremov <yefremov.denis@gmail.com>
2015-06-18 21:03:43 +0300

    dismember: exec plugin

---
+++ lib/App/Dismember/Plugin/Exec.pm
@@ -0,0 +1,77 @@
+package App::Dismember::Plugin::Exec;
+
+use warnings;
+use strict;
+
+use Getopt::Long qw(:config gnu_compat permute no_getopt_compat pass_through);
+
+sub process_options
+{
+   my ($self, $config) = @_;
+   my $file;
+   my $wait = 1;
+
+   GetOptions(
+      'plugin-exec-file=s' => \$file,
+      'plugin-exec-wait!'  => \$wait,
+   ) or die("Error in command line arguments\n");
+
+   die "Option --plugin-exec-file should be provided.\n"
+      unless $file;
+
+   unless (-f $file && -r _ && -x _) {
+      die "FAIL: Can't access file $file.\n"
+   }
+
+   $config->{'exec-file'} = $file;
+
+   bless { file => $file, wait => $wait }, $self
+}
+
+sub priority
+{
+   99
+}
+
+sub level
+{
+   'post_output'
+}
+
+sub action
+{
+   my ($self, $opts) = @_;
+
+   return undef
+      unless exists $opts->{'dir'};
+
+   my $pid = fork();
+   die "FAIL: can't fork $!"
+      unless defined $pid;
+
+   unless ($pid) {
+      my %args;
+      foreach (keys %$opts) {
+         unless (ref $opts->{$_}) {
+            $args{'--' . $_} = $opts->{$_}
+         }
+      }
+      print "EXEC: $self->{file} @{[%args]}\n";
+
+      open (STDIN,  '</dev/null');
+      unless ($self->{wait}) {
+         open (STDOUT, '>/dev/null');
+         open (STDERR, '>&STDOUT');
+      }
+      exec($self->{file}, %args);
+   }
+
+   if ($self->{wait}) {
+      waitpid $pid, 0
+   }
+
+   undef
+}
+
+
+1;
