0b270ab80f32eb1ce2d29f05fac9a8cc6d501537
Denis Efremov <yefremov.denis@gmail.com>
2015-06-24 18:30:24 +0300

    scripts/framac-why3typecheck.sh

---
+++ script/framac-why3typecheck.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+while (( $# > 0 ))
+do
+   case "$1" in
+      --dir)
+         dir=$2
+         break
+         ;;
+      *)
+         shift
+         ;;
+   esac
+done
+
+if [[ -z "$dir" || ! -r "$dir/module.c" ]]
+then
+   exit 1
+fi
+
+frama-c -jessie -jessie-target why3typecheck "$dir/module.c"
