b92961dd76a719aa69cc15d05858a63cc08ae3ad
Denis Efremov <yefremov.denis@gmail.com>
2015-07-02 18:25:23 +0300

    dismember: plugin: include: link support

--- lib/App/Dismember/Plugin/Include.pm
+++ lib/App/Dismember/Plugin/Include.pm
@@ -8,14 +8,17 @@ use Kernel::Module::Graph;
 use File::Basename;
 use File::Spec::Functions qw(catfile);
 use File::Copy;
+use Cwd qw(abs_path);
 
 sub process_options
 {
    my ($self, $config) = @_;
    my @include;
+   my $link = 1;
 
    GetOptions(
-      'plugin-include-file=s' => \@include
+      'plugin-include-file=s' => \@include,
+      'plugin-include-link!'  => \$link,
    ) or die("Error in command line arguments\n");
 
    die "Option --plugin-include-file should be provided.\n"
@@ -40,12 +43,12 @@ sub process_options
 
    $config->{'include'} = \%include;
 
-   bless { include => \%include, base_dir => $config->{output_dir} }, $self
+   bless { include => \%include, base_dir => $config->{output_dir}, link => $link }, $self
 }
 
 sub priority
 {
-   99
+   90
 }
 
 sub level
@@ -63,9 +66,15 @@ sub action
    foreach my $area (keys %{$self->{include}}) {
       foreach (@{$self->{include}{$area}}) {
          my $include = basename $_;
-         my $dst = catfile($opts->{output_dir}, $include);
-         copy($_, $dst);
-         print "plugin: include: copy $_ -> $dst\n";
+         my $old = abs_path $_;
+         my $new = catfile($opts->{output_dir}, $include);
+         if ($self->{link}) {
+            symlink $old, $new;
+            print "plugin: include: link $old -> $new\n";
+         } else {
+            copy($old, $new);
+            print "plugin: include: copy $old -> $new\n";
+         }
 
          $opts->{'output'}{$area} = qq(#include "$include"\n\n) .
                                  $opts->{'output'}{$area};
