ddf047027732c184f1450b870c060e3d2cc7d085
Denis Efremov <yefremov.denis@gmail.com>
2015-07-24 23:45:43 +0300

    dismember: plugin: exec: documentation

--- lib/App/Dismember/Plugin/Exec.pm
+++ lib/App/Dismember/Plugin/Exec.pm
@@ -1,22 +1,63 @@
 package App::Dismember::Plugin::Exec;
 
+use Pod::Usage;
+use Pod::Find qw(pod_where);
 use warnings;
 use strict;
 
 use Getopt::Long qw(:config gnu_compat permute no_getopt_compat pass_through);
 
+=encoding utf8
+
+=pod
+
+=head1 Plugin::Exec
+
+Plugin::Exec - плагин для запуска программы после вывода результатов работы dismember для конкретной функции
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--plugin-exec-file path>
+
+Путь до исполняемого файла. В качестве аргументов командной строки передаются все зарегестрированные опции предыдущих плагинов и опция --dir path, где path - это путь до директории с файлами.
+
+=item B<--[no-]plugin-exec-wait>
+
+Ожидать или не ожидать конца исполнения вызываемой программы. По умолчанию плагин ожидает конца выполнения запускаемой программы.
+
+=item B<--plugin-exec-help>
+
+Выводит полное описание плагина.
+
+=back
+
+=cut
+
+
 sub process_options
 {
    my ($self, $config) = @_;
    my $file;
+   my $help = 0;
    my $wait = 1;
 
    GetOptions(
       'plugin-exec-file=s' => \$file,
       'plugin-exec-wait!'  => \$wait,
+      'plugin-exec-help'   => \$help,
    ) or die("Error in command line arguments\n");
 
-   die "Option --plugin-exec-file should be provided.\n"
+   my $input = pod_where({-inc => 1}, __PACKAGE__);
+   pod2usage({ -input   => $input,
+               -verbose => 2,
+               -exitval => 0 })
+       if $help;
+
+   pod2usage({ -input => $input,
+               -msg => "Option --plugin-exec-file should be provided.\n",
+               -exitval => 1 })
       unless $file;
 
    unless (-f $file && -r _ && -x _) {
