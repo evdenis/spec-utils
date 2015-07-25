d28edd721d325ab65220daca993cb826ef22609f
Denis Efremov <yefremov.denis@gmail.com>
2015-07-25 11:42:46 +0300

    dismember: plugin: rewrite: documentation

--- lib/App/Dismember/Plugin/Rewrite.pm
+++ lib/App/Dismember/Plugin/Rewrite.pm
@@ -3,20 +3,61 @@ package App::Dismember::Plugin::Rewrite;
 use warnings;
 use strict;
 
+use Pod::Usage;
+use Pod::Find qw(pod_where);
 use Getopt::Long qw(:config gnu_compat permute no_getopt_compat pass_through);
+
+=encoding utf8
+
+=pod
+
+=head1 Plugin::Rewrite
+
+Plugin::Rewrite - плагин для переписывания определений структур, функций, макросов и т. д.
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--plugin-rewrite-id string>
+
+Переписать определение сущности из string. string имеет формат 'entity_name^new_entity_definition'
+
+=item B<--[no-]plugin-rewrite-reduced>
+
+Опция определяет уровень работы плагина. Для всего графа либо для урезанного графа конкретной функции. По умолчанию, когда dismember запускается с несколькими функциями в опции --functions, плагин работает на уровне всего графа, в обратном случае на уровне урезанного графа конкретной функции. Опция влияет на быстродействие.
+
+=item B<--plugin-rewrite-help>
+
+Выводит полное описание плагина.
+
+=back
+
+=cut
+
 
 sub process_options
 {
    my ($self, $config) = @_;
    my @rewrite;
+   my $help = 0;
    my $reduced = @{$config->{functions}} > 1 || $config->{all} ? 0 : 1;
 
    GetOptions(
       'plugin-rewrite-id=s'     => \@rewrite,
-      'plugin-rewrite-reduced!' => \$reduced
+      'plugin-rewrite-reduced!' => \$reduced,
+      'plugin-rewrite-help'     => \$help,
    ) or die("Error in command line arguments\n");
 
-   die "Option --plugin-rewrite-id should be provided.\n"
+   my $input = pod_where({-inc => 1}, __PACKAGE__);
+   pod2usage({ -input   => $input,
+               -verbose => 2,
+               -exitval => 0 })
+       if $help;
+
+   pod2usage({ -input => $input,
+               -msg => "Option --plugin-rewrite-id should be provided.\n",
+               -exitval => 1 })
       unless @rewrite;
 
    my %rewrite;
