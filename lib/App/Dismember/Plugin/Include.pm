4da57321d695ea6348da81828e8575036acc36c8
Denis Efremov <yefremov.denis@gmail.com>
2015-07-25 10:49:11 +0300

    dismember: plugin: include: documentation

--- lib/App/Dismember/Plugin/Include.pm
+++ lib/App/Dismember/Plugin/Include.pm
@@ -3,6 +3,8 @@ package App::Dismember::Plugin::Include;
 use warnings;
 use strict;
 
+use Pod::Usage;
+use Pod::Find qw(pod_where);
 use Getopt::Long qw(:config gnu_compat permute no_getopt_compat pass_through);
 use Kernel::Module::Graph;
 use File::Basename;
@@ -10,18 +12,57 @@ use File::Spec::Functions qw(catfile);
 use File::Copy;
 use Cwd qw(abs_path);
 
+=encoding utf8
+
+=pod
+
+=head1 Plugin::Include
+
+Plugin::Include - плагин для добавления директивы #include <file.h> в выводимые файлы
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--plugin-include-file string>
+
+В string содержится указание на то, в какой файл добавлять директиву #include и путь до самого файла. string имеет формат 'file^path', где file - это kernel_h(1), external_h(2), module_h(3), module_c(4), а path - это путь до подключаемого файла. Плагин копирует файл из path в директорию к остальным файлам и дописывает '#include "$(basename path)"' в файл area.
+
+=item B<--plugin-include-link>
+
+Изменяет поведение опции plugin-include-file. Вместо копирования подключаемого файла происходит создания симлинка на него.
+
+=item B<--plugin-include-help>
+
+Выводит полное описание плагина.
+
+=back
+
+=cut
+
+
 sub process_options
 {
    my ($self, $config) = @_;
    my @include;
+   my $help = 0;
    my $link = 1;
 
    GetOptions(
       'plugin-include-file=s' => \@include,
       'plugin-include-link!'  => \$link,
+      'plugin-include-help'   => \$help,
    ) or die("Error in command line arguments\n");
 
-   die "Option --plugin-include-file should be provided.\n"
+   my $input = pod_where({-inc => 1}, __PACKAGE__);
+   pod2usage({ -input   => $input,
+               -verbose => 2,
+               -exitval => 0 })
+       if $help;
+
+   pod2usage({ -input => $input,
+               -msg => "Option --plugin-include-file should be provided.\n",
+               -exitval => 1 })
       unless @include;
 
    my %include;
