4ed12f5b4f5f8d5212fb5f35b1002ae659a7e341
Denis Efremov <yefremov.denis@gmail.com>
2015-08-24 16:34:54 +0300

    lib: C: Global: type && initialized update

--- lib/C/Global.pm
+++ lib/C/Global.pm
@@ -4,14 +4,33 @@ use namespace::autoclean;
 
 extends 'C::Entity';
 
+has 'initialized' => (
+   is      => 'ro',
+   isa     => 'Bool',
+   lazy    => 1,
+   builder => '_build_initialized'
+);
+
+has 'type' => (
+   is => 'rw',
+   isa => 'Str',
+   required => 1
+);
+
+
+sub _build_initialized
+{
+   index($_[0]->code, '=') != -1
+}
+
 sub to_string
 {
    my $code = $_[0]->code;
 
-   if ((my $i = index($code, '=')) != -1) {
-      $code = substr($code, 0, $i) . ';';
-      $code =~ s/^static/extern/;
-   }
+   #if ((my $i = index($code, '=')) != -1) {
+   #   $code = substr($code, 0, $i) . ';';
+   #   #$code =~ s/^static/extern/; Do we really need it?
+   #}
 
    $code
 }
