6c942271bf6887f914a4015350ffd4cfabaa1734
Denis Efremov <yefremov.denis@gmail.com>
2016-11-25 13:15:19 +0300

    add quotes

--- lib/App/Graph.pm
+++ lib/App/Graph.pm
@@ -367,7 +367,7 @@ CACHE: if ($args->{cache}) {
          push @legenda, qq(    color = "lightgrey";\n);
          push @legenda, qq(    label = "Priority levels";\n);
          push @legenda, qq(    node [shape = "box", style = "filled"];\n);
-         push @legenda, '    ' . join(' -> ', (1 .. @{$args->{config}{priority}{lists}}) ) . ' [ style = "invis" ];' . "\n";
+         push @legenda, '    ' . join(' -> ', map { "\"$_\"" } (1 .. @{$args->{config}{priority}{lists}})) . ' [ style = "invis" ];' . "\n";
          my @colors = map { $args->{config}{priority}{colors}{$_} } @{ $args->{config}{priority}{lists} };
          while (my ($idx, $color) = each @colors) {
             ++$idx;
