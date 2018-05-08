9a81603c6076766d368990a10f0af77d21bebd01
Denis Efremov <efremov@ispras.ru>
2018-05-08 16:05:15 +0300

    add compile script

---
+++ scripts/compile.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+while (( $# > 0 ))
+do
+   case "$1" in
+      --dir)
+         dir=$2
+         shift
+         ;;
+      --file)
+         file=$2
+         shift
+         ;;
+      *)
+         shift
+         ;;
+   esac
+done
+
+if [[ -z "$file" || ! -r "$file" ]]
+then
+   exit 1
+fi
+
+gcc -c -w "$file"
